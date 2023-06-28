461d46d08b77ebda773f64bc428300875b0d0ff9
Hecater88 <47868066+Hecater88@users.noreply.github.com>
2023-06-28 14:03:06 +0200

    Project setup

---
+++ app/layout.tsx
@@ -0,0 +1,17 @@
+import { PropsWithChildren } from "react";
+export const metadata = {
+	title: "Comparador de Pienso",
+	description: "Elige el pienso perfecto para tu mascota",
+};
+
+const RootLayout = ({ children }: PropsWithChildren<{}>) => {
+	return (
+		<html lang="es">
+			<body>
+				<main className="app">{children}</main>
+			</body>
+		</html>
+	);
+};
+
+export default RootLayout;
